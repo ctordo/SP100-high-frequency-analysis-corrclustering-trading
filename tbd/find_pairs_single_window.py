a94182c7c0a691172115c7884ceba66c819ac812
Cyprien Tordo <cyprien.tordo@epfl.ch>
2026-01-25 14:18:01 +0100

    deleted old .py files and deleted individual notebooks

--- tbd/find_pairs_single_window.py
+++
@@ -1,9 +0,0 @@
-from covariances import run_covariance_denoise
-from covariances import CovarianceDenoiser
-
-class PairsSingleWindow() : 
-    def __init__(self,ret_window, cov_window, tickers, nb_pairs) : 
-        self.ret_window = ret_window
-        self.cov_window = cov_window
-        self.tickers = tickers 
-        self.nb_pairs = nb_pairs
